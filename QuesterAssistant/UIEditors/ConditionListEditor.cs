37805a3b69e7fada2a290cae39d769627e78c377
michael_prog <michael_prog@mail.ru>
2019-10-09 15:40:45 +0300

    Добавил команды MoveToSea (реализация FreeFire). Добавил условия PlayerParagon, CheckNode, ComplexCondition.

---
+++ QuesterAssistant/UIEditors/ConditionListEditor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms;
+using Astral.Quester.Classes;
+using DevExpress.XtraEditors.Controls;
+using QuesterAssistant.UIEditors.Forms;
+using MyNW.Classes;
+using ConditionList = System.Collections.Generic.List<Astral.Quester.Classes.Condition>;
+
+
+namespace QuesterAssistant.UIEditors
+{
+    class ConditionListEditor : UITypeEditor
+    {
+        //internal static ConditionListForm listEditor = null;
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            ConditionListForm listEditor = new ConditionListForm();
+
+            ConditionList newConditions = listEditor.GetConditionList(value as ConditionList);
+
+            if (listEditor.DialogResult == DialogResult.OK)
+            {
+                return newConditions;
+            }            
+            return value;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+    }
+}
---
+++ QuesterAssistant/UIEditors/ParagonListEditor.cs
@@ -0,0 +1,27 @@
+﻿using QuesterAssistant.Classes;
+using QuesterAssistant.UIEditors.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms;
+
+namespace QuesterAssistant.UIEditors
+{
+    class ParagonSelectEditor : UITypeEditor
+    {
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            SelectedParagons paragons = PlayerParagonSelectForm.GetParagons(value as SelectedParagons);
+
+            if (paragons != null)
+                return paragons;
+            return value;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+    }
+}
