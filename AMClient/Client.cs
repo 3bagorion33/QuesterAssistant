943906878f154c53acd3c6bd50244f619e6632fe
Orion33 <a15@bk.ru>
2019-11-08 11:53:17 +0300

    Add AMClient dummy

---
+++ AMClient/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace AMClient
+{
+    public class Client
+    {
+        public Client(string PrivateKey) { }
+        public bool Connect() => true;
+        public void Disconnect() { }
+        public void EnableAutoReconnect() { }
+        public bool CheckPrivateKey() => true;
+        public bool IsProductActive(string ProductName) =>
+            ProductName != "Astral Premium Package" && ProductName == "Astral Elite Package";
+        public int GetSessionAmount(string ProductName) => 0;
+        public bool StartSession(string ProductName) => true;
+        public bool MultiIPCheck(string ProductName) => false;
+	}
+}
