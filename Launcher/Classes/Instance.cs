3f02ec2282a92726015bb8b438fd76e618b79bf3
Orion33 <a15@bk.ru>
2019-11-15 18:01:11 +0300

    Add patch for Patrol

--- Launcher/Classes/Instance.cs
+++ Launcher/Classes/Instance.cs
@@ -3,7 +3,6 @@ using QuesterAssistant.Classes.Common;
 using QuesterAssistant.Classes.Extensions;
 using QuesterAssistant.Panels;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -21,78 +20,8 @@ namespace Launcher.Classes
         public string NewTitle { get; set; } = string.Empty;
         private const int HASH_SIZE = 4;
         
-        // Astral.Forms.Main.method_5
-        private readonly List<byte[]> REWRITE_TITLE_ORIG = new List<byte[]>
-        {
-            new byte[] { 0x1B, 0x30, 0x04, 0x00, 0x46, 0x01, 0x00, 0x00 },
-            new byte[] { 0x06, 0x6F, 0x17, 0x01, 0x00, 0x0A, 0x28, 0xD2 },
-            new byte[] { 0x18, 0x06, 0x72, 0x1F, 0x89, 0x01, 0x70, 0x7D}
-        };
-
-        private readonly List<byte[]> REWRITE_TITLE_NEW = new List<byte[]>
-        {
-            new byte[] { 0x1B, 0x30, 0x0C, 0x00, 0x46, 0x01, 0x00, 0x00 },
-            new byte[] { 0x06, 0x6F, 0xB0, 0x01, 0x00, 0x0A, 0x28, 0xD2 },
-            new byte[] { 0x18, 0x06, 0x7E, 0x63, 0x00, 0x00, 0x0A, 0x7D}
-        };
-
-        // AssemblyInfo
-        private readonly List<byte[]> ASSEMBLYINFO_ORIG = new List<byte[]>
-        {
-            new byte[] { 0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x00, 0x00 },
-            new byte[] { 0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x2E, 0x00, 0x65, 0x00, 0x78, 0x00, 0x65, 0x00 },
-            new byte[] { 0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x2E, 0x00, 0x65, 0x00, 0x78, 0x00, 0x65, 0x00 },
-            new byte[] { 0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x00, 0x00 },
-        };
-
-        private readonly List<byte[]> ASSEMBLYINFO_NEW = new List<byte[]>
-        {
-            new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
-            new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
-            new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
-            new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
-        };
-
-        // Astral.Professions.FSM.States.Main.Run
-        private readonly List<byte[]> PRAY_PAUSE_ORIG = new List<byte[]>
-        {
-            new byte[] { 0x06, 0x1B, 0x58, 0x28 },
-            new byte[] { 0x1B, 0x30, 0x03, 0x00, 0x24, 0x07, 0x00, 0x00 },
-            new byte[] { 0x2C, 0xDB, 0x18, 0x1A }
-        };
-
-        private readonly List<byte[]> PRAY_PAUSE_NEW = new List<byte[]>
-        {
-            new byte[] { 0x06, 0x16, 0x58, 0x28 },
-            new byte[] { 0x1B, 0x30, 0x0B, 0x00, 0x24, 0x07, 0x00, 0x00 },
-            new byte[] { 0x2C, 0xDB, 0x16, 0x16 }
-        };
-
-        // Patrol
-        private readonly List<byte[]> PATROL_PAUSE_ORIG = new List<byte[]>
-        {
-            new byte[] { 0x5E, 0x02, 0x20, 0x10, 0x27, 0x00, 0x00, 0x73 },
-            new byte[] { 0x5E, 0x02, 0x20, 0x10, 0x27, 0x00, 0x00, 0x73 },
-        };
-        private readonly List<byte[]> PATROL_PAUSE_NEW = new List<byte[]>
-        {
-            new byte[] { 0x5E, 0x02, 0x20, 0xC8, 0x00, 0x00, 0x00, 0x73 },
-            new byte[] { 0x5E, 0x02, 0x20, 0xC8, 0x00, 0x00, 0x00, 0x73 },
-        };
-
         public Instance()
         {
-            bool ByteArraysEqual(byte[] b1, byte[] b2)
-            {
-                if (b1 == b2) return true;
-                if (b1 == null || b2 == null) return false;
-                if (b1.Length != b2.Length) return false;
-                for (int i = 0; i < b1.Length; i++)
-                {
-                    if (b1[i] != b2[i]) return false;
-                }
-                return true;
-            }
             HashChanged += Delete;
             using (MD5 md5Hash = MD5.Create())
             {
@@ -100,39 +29,7 @@ namespace Launcher.Classes
                 try
                 {
                     File.Copy("Astral.exe", procName);
-
-                    void Rewrite(List<byte[]> origBytes, List<byte[]> newBytes)
-                    {
-                        using (FileStream stream = File.Open(procName, FileMode.Open, FileAccess.ReadWrite))
-                        {
-                            for (int i = 0; i < origBytes.Count; i++)
-                            {
-                                byte[] reading = new byte[origBytes[i].Length];
-                                stream.Position = 0;
-                                while (stream.Read(reading, 0, 1) > 0)
-                                {
-                                    if (origBytes[i][0] == reading[0])
-                                    {
-                                        stream.Position--;
-                                        stream.Read(reading, 0, origBytes[i].Length);
-                                        if (ByteArraysEqual(origBytes[i], reading))
-                                        {
-                                            stream.Position = stream.Position - origBytes[i].Length;
-                                            break;
-                                        }
-                                    }
-                                }
-
-                                if (stream.Length > stream.Position + origBytes[i].Length)
-                                    stream.Write(newBytes[i], 0, newBytes[i].Length);
-                            }
-                        }
-                    }
-
-                    Rewrite(REWRITE_TITLE_ORIG, REWRITE_TITLE_NEW);
-                    Rewrite(ASSEMBLYINFO_ORIG, ASSEMBLYINFO_NEW);
-                    Rewrite(PRAY_PAUSE_ORIG, PRAY_PAUSE_NEW);
-                    Rewrite(PATROL_PAUSE_ORIG, PATROL_PAUSE_NEW);
+                    Patches.Apply(procName);
                     Process = Process.Start(procName);
                 }
                 catch (Exception)
@@ -200,7 +97,7 @@ namespace Launcher.Classes
 
         public void Delete()
         {
-            while (Process.HasExited && File.Exists($"{Process.ProcessName}.exe"))
+            while ((Process?.HasExited ?? true) && File.Exists($"{Process?.ProcessName}.exe"))
             {
                 Thread.Sleep(100);
                 try
---
+++ Launcher/Classes/Patches.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Launcher.Classes
+{
+    sealed class Patches
+    {
+        private static readonly List<Patch> REWRITE_TITLE = new List<Patch> // Astral.Forms.Main.method_5
+        {
+            //new Patch
+            //{
+            //    Orig = new byte[] {0x1B, 0x30, 0x04, 0x00, 0x46, 0x01, 0x00, 0x00},
+            //    Ptch = new byte[] {0x1B, 0x30, 0x0C, 0x00, 0x46, 0x01, 0x00, 0x00},
+            //},
+            new Patch
+            {
+                Orig = new byte[] {0x06, 0x6F, 0x17, 0x01, 0x00, 0x0A, 0x28, 0xD2},
+                Ptch = new byte[] {0x06, 0x6F, 0xB0, 0x01, 0x00, 0x0A, 0x28, 0xD2},
+            },
+            new Patch
+            {
+                Orig = new byte[] {0x18, 0x06, 0x72, 0x1F, 0x89, 0x01, 0x70, 0x7D},
+                Ptch = new byte[] {0x18, 0x06, 0x7E, 0x63, 0x00, 0x00, 0x0A, 0x7D}
+            }
+        };
+        
+        private static readonly List<Patch> ASSEMBLYINFO = new List<Patch> // AssemblyInfo
+        {
+            new Patch
+            {
+                Orig = new byte[] {0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x00, 0x00, 0x00, 0x00},
+                Ptch = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+
+            },
+            new Patch
+            {
+                Orig = new byte[] {0x41, 0x00, 0x73, 0x00, 0x74, 0x00, 0x72, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x2E, 0x00, 0x65, 0x00,0x78, 0x00, 0x65, 0x00},
+                Ptch = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00, 0x00, 0x00, 0x00},
+
+            },
+        };
+
+        private static readonly List<Patch> PRAY_PAUSE = new List<Patch> // Astral.Professions.FSM.States.Main.Run
+        {
+            new Patch
+            {
+                Orig = new byte[] {0x06, 0x1B, 0x58, 0x28},
+                Ptch = new byte[] {0x06, 0x16, 0x58, 0x28},
+
+            },
+            //new Patch
+            //{
+            //    Orig = new byte[] {0x1B, 0x30, 0x03, 0x00, 0x24, 0x07, 0x00, 0x00},
+            //    Ptch = new byte[] {0x1B, 0x30, 0x0B, 0x00, 0x24, 0x07, 0x00, 0x00},
+            //},
+            new Patch
+            {
+                Orig = new byte[] {0x2C, 0xDB, 0x18, 0x1A},
+                Ptch = new byte[] {0x2C, 0xDB, 0x16, 0x16}
+            }
+        };
+
+        private static readonly List<Patch> PATROL_PAUSE = new List<Patch> // Patrol
+        {
+            new Patch // Astral.Quester.FSM.States.CheckAction.get_NeedToRun()
+            {
+                Orig = new byte[] {0x06, 0x02, 0x20, 0x10, 0x27, 0x00, 0x00, 0x73},
+                Ptch = new byte[] {0x06, 0x02, 0x20, 0xC8, 0x00, 0x00, 0x00, 0x73}
+            }
+        };
+
+        private static void Rewrite(ref byte[] stream, List<Patch> patches)
+        {
+            IEnumerable<int> Search(byte[] src, byte[] pattern)
+            {
+                int c = src.Length - pattern.Length + 1;
+                int j;
+                for (int i = 0; i < c; i++)
+                {
+                    if (src[i] != pattern[0]) continue;
+                    for (j = pattern.Length - 1; j >= 1 && src[i + j] == pattern[j]; j--) ;
+                    if (j == 0) yield return i;
+                }
+            }
+
+            foreach (Patch patch in patches)
+            {
+                var idx3 = Search(stream, patch.Orig);
+                foreach (var i in idx3)
+                {
+                    for (int j = 0; j < patch.Orig.Length; j++)
+                    {
+                        stream[i + j] = patch.Ptch[j];
+                    }
+                }
+            }
+        }
+
+        public static void Apply(string procName)
+        {
+            var stream = File.ReadAllBytes(procName);
+            typeof(Patches).GetFields(BindingFlags.NonPublic | BindingFlags.Static)
+                .ToList()
+                .FindAll(l => l.FieldType == typeof(List<Patch>))
+                .ForEach(l => Rewrite(ref stream, l.GetValue(new Patches()) as List<Patch>));
+            File.WriteAllBytes(procName, stream);
+        }
+
+        private class Patch
+        {
+            public byte[] Orig;
+            public byte[] Ptch;
+        }
+    }
+}
