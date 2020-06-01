65324d7a06fab3e75d088f1d6ad033b0a4bc16fc
Orion33 <a15@bk.ru>
2020-06-01 17:22:31 +0300

    Add ADSTool

---
+++ ADSTool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ADSTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var sourceFileName = args[0];
+                var targetFileName = args[1];
+                using (FileStream targetStream = FileStreamHelper.OpenWithStream(targetFileName, FileMode.Create, FileAccess.Write))
+                {
+                    using (FileStream sourceStream = FileStreamHelper.OpenWithStream(sourceFileName, FileMode.Open, FileAccess.Read))
+                    {
+                        var buff = new byte[sourceStream.Length];
+                        var length = (int) sourceStream.Length;
+                        sourceStream.Read(buff, 0, length);
+                        targetStream.Write(buff, 0, length);
+                    }
+                }
+            }
+        }
+    }
+}
